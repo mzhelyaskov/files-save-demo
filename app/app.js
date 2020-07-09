2ba80037387fecd585f3ae152036a75516f4bb42
mykhailo.zheliaskov <mykhailo.zheliaskov@decerto.pl>
2020-07-09 08:19:31 +0200

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const mime = require('mime');
+const urlUtils = require("url");
+const bodyParser = require('body-parser')
+const fs = require('fs')
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.get('/download', function (req, res) {
+	const file = `${__dirname}/documents/semantic_versioning.pdf`;
+	const pathParams = urlUtils.parse(req.url, true).query;
+	if (pathParams.octetStream === 'true') {
+		const filename = path.basename(file);
+		res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+		res.setHeader('Content-Type', 'application/octet-stream');
+		res.send(file);
+	} else {
+		res.download(file);
+	}
+});
+
+app.post('/log', function (req, res) {
+	const file = `${__dirname}/logs/terminal-log.txt`;
+	fs.appendFile(file, req.body.terminalLog, function (error) {
+		if (error) {
+			throw error;
+		}
+		res.status(200).json({message: "File successfully written"});
+	});
+});
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
